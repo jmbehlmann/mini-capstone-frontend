3d633669bd8323f68378409aabb673a8904965a9
jmbehlmann <110798325+jmbehlmann@users.noreply.github.com>
2023-12-21 11:51:17 -0600

    made index action

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,7 +1,25 @@
+import axios from 'axios';
+import { useState, useEffect } from "react";
+import { ProductsIndex } from "./ProductsIndex";
+
 export function Content() {
+
+  const [products, setProducts] = useState([]);
+
+  const handleIndexProducts = () => {
+    console.log("handleIndexProducts");
+    axios.get("http://localhost:3000/products.json").then((response) => {
+      console.log(response.data);
+      setProducts(response.data);
+    });
+  };
+
+  useEffect(handleIndexProducts)
+
   return (
     <main>
-      <h1>Welcome to React!</h1>
+      <h1>Welcome to This Mess!</h1>
+      <ProductsIndex products={products} />
     </main>
   )
 }
---
+++ src/ProductsIndex.jsx
@@ -0,0 +1,15 @@
+export function ProductsIndex(props) {
+  return (
+    <div>
+      <h1>All products</h1>
+      {props.products.map((products) => (
+        <div key={products.id}>
+          <h2>{products.name}</h2>
+          <p>{products.description}</p>
+          <p>Price: {products.price}</p>
+          <p>Inventory: {products.inventory}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
