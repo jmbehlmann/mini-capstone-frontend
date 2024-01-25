502594d5687f8c236ec7f8f0567dd3705f6f2bb1
jmbehlmann <110798325+jmbehlmann@users.noreply.github.com>
2024-01-25 13:08:54 -0600

    removed horrific navbar

--- src/Header.jsx
+++ src/Header.jsx
@@ -5,85 +5,79 @@ export function Header() {
 
   if (localStorage.jwt) {
    loggedInStatus = (
-    <>
-      <li className="nav-item">
-        <NavLink className="nav-link" to="/logout">Log Out</NavLink>
-      </li>
-    </>
+    <Link to="/logout">Log Out</Link>
+
+
+
+    // <>
+    //   <li className="nav-item">
+    //     <NavLink className="nav-link" to="/logout">Log Out</NavLink>
+    //   </li>
+    // </>
     )
   } else {
     loggedInStatus = (
-    <>
-      <li className="nav-item">
-        <NavLink className="nav-link" to="/login" >Login</NavLink>
-      </li>
-      <li className="nav-item">
-        <NavLink className="nav-link" to="/signup">Sign Up!</NavLink>
-      </li>
-    </>
+      <>
+        <Link to="/login">Login</Link> | <Link to="/signup">Sign Up</Link>
+      </>
+
+
+
+    // <>
+    //   <li className="nav-item">
+    //     <NavLink className="nav-link" to="/login" >Login</NavLink>
+    //   </li>
+    //   <li className="nav-item">
+    //     <NavLink className="nav-link" to="/signup">Sign Up!</NavLink>
+    //   </li>
+    // </>
     )
   }
 
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="/">Navi-Gator</a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="/">Home</a>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/cart">Cart</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/orders">Orders</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/newproduct">New Product</NavLink>
-            </li>
-            {/* <li className="nav-item">
-              <NavLink className="nav-link" to="/chart">You want a chart?</NavLink>
-            </li> */}
+    <header>
+      <nav>
+       <Link to="/">Home</Link> | <Link to="/newproduct">New Product</Link> | <Link to="/cart">Cart</Link> | <Link to="/orders">Orders</Link> | {loggedInStatus} |
+      </nav>
+    </header>
 
-            {loggedInStatus}
 
-            {/* <li className="nav-item">
-              <NavLink className="nav-link" to="/login" >Login</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/signup">Sign Up!</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/logout">Log Out</NavLink>
-            </li> */}
 
-            {/* <li className="nav-item dropdown">
-              <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Dropdown
-              </a>
-              <ul className="dropdown-menu">
-                <li><a className="dropdown-item" href="#">Action</a></li>
-                <li><a className="dropdown-item" href="#">Another action</a></li>
-                <li><hr className="dropdown-divider" /></li>
-                <li><a className="dropdown-item" href="#">Something else here</a></li>
-              </ul>
-            </li> */}
 
-            {/* <li className="nav-item">
-              <a className="nav-link disabled" aria-disabled="true">Don't click it</a>
-            </li> */}
+    // <nav className="navbar navbar-expand-lg bg-body-tertiary">
+    //   <div className="container-fluid">
+    //     <a className="navbar-brand" href="/">Navi-Gator</a>
+    //     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    //       <span className="navbar-toggler-icon"></span>
+    //     </button>
+    //     <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    //       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+    //         <li className="nav-item">
+    //           <a className="nav-link active" aria-current="page" href="/">Home</a>
+    //         </li>
+    //         <li className="nav-item">
+    //           <NavLink className="nav-link" to="/cart">Cart</NavLink>
+    //         </li>
+    //         <li className="nav-item">
+    //           <NavLink className="nav-link" to="/orders">Orders</NavLink>
+    //         </li>
+    //         <li className="nav-item">
+    //           <NavLink className="nav-link" to="/newproduct">New Product</NavLink>
+    //         </li>
+    //         {/* <li className="nav-item">
+    //           <NavLink className="nav-link" to="/chart">You want a chart?</NavLink>
+    //         </li> */}
 
-          </ul>
-          <form className="d-flex" role="search">
-            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-            <button className="btn btn-outline-success" type="submit">Search</button>
-          </form>
-        </div>
-      </div>
-    </nav>
+    //         {loggedInStatus}
+
+
+    //       </ul>
+    //       <form className="d-flex" role="search">
+    //         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+    //         <button className="btn btn-outline-success" type="submit">Search</button>
+    //       </form>
+    //     </div>
+    //   </div>
+    // </nav>
   )
 }
